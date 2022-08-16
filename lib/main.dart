3328f8ba5859e227e2c8e42f02a4ab9450cf21ce
Akinwale Damilare <damilareakinwale0@gmail.com>
2022-08-16 04:05:47 +0100

    calculator using set state

--- lib/main.dart
+++ lib/main.dart
@@ -17,24 +17,17 @@ class CalculatorScreen extends StatefulWidget {
 class _CalculatorScreenState extends State<CalculatorScreen> {
   int? firstNum;
   int? secondNum;
-  String? history;
-  String? textToDisplay;
+  String? history = '00';
+  String? textToDisplay = '0';
   String? res;
   String? operation;
 
   void btnOnClick(String btnVal) {
-    print(btnVal);
     if (btnVal == 'C') {
       textToDisplay = '';
       firstNum = 0;
       secondNum = 0;
       res = '';
-    } else if (btnVal == 'AC') {
-      textToDisplay = '';
-      firstNum = 0;
-      secondNum = 0;
-      res = '';
-      history = '';
     } else if (btnVal == '<') {
       res = textToDisplay?.substring(0, textToDisplay!.length - 1);
     } else if (btnVal == '+' ||
@@ -44,7 +37,7 @@ class _CalculatorScreenState extends State<CalculatorScreen> {
       firstNum = int.parse(textToDisplay!);
       res = '';
       operation = btnVal;
-    } else if (btnVal == '*') {
+    } else if (btnVal == '=') {
       secondNum = int.parse(textToDisplay!);
       if (operation == '+') {
         res = (firstNum! + secondNum!).toString();
@@ -77,13 +70,14 @@ class _CalculatorScreenState extends State<CalculatorScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(title: const Text('Calculator')),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.end,
         children: [
           Container(
             padding: const EdgeInsets.all(12),
             alignment: const Alignment(1.0, 1.0),
-            child: Text(history!,
+            child: Text(history ?? '00',
                 style: GoogleFonts.rubik(
                     textStyle:
                         const TextStyle(fontSize: 24, color: Colors.blueGrey))),
@@ -91,11 +85,115 @@ class _CalculatorScreenState extends State<CalculatorScreen> {
           Container(
             padding: const EdgeInsets.all(12),
             alignment: const Alignment(1.0, 1.0),
-            child: Text(textToDisplay!,
+            child: Text(textToDisplay ?? '0',
                 style: GoogleFonts.rubik(
                     textStyle:
                         const TextStyle(fontSize: 48, color: Colors.blueGrey))),
           ),
+          Row(children: [
+            CalculatorButton(
+              buttonText: 'C',
+              buttonOnPressed: btnOnClick,
+              buttonColor: Colors.yellow.shade200,
+              textColor: Colors.black,
+            ),
+            CalculatorButton(
+              buttonText: '(',
+              buttonOnPressed: btnOnClick,
+              buttonColor: Colors.yellow.shade200,
+              textColor: Colors.black,
+            ),
+            CalculatorButton(
+              buttonText: ')',
+              buttonOnPressed: btnOnClick,
+              buttonColor: Colors.yellow.shade200,
+              textColor: Colors.black,
+            ),
+            CalculatorButton(
+              buttonText: '*',
+              buttonOnPressed: btnOnClick,
+              buttonColor: Colors.purpleAccent,
+              textColor: Colors.black,
+            )
+          ]),
+          Row(children: [
+            CalculatorButton(
+              buttonText: '7',
+              buttonOnPressed: btnOnClick,
+              buttonColor: Colors.white,
+              textColor: Colors.black,
+            ),
+            CalculatorButton(
+              buttonText: '8',
+              buttonOnPressed: btnOnClick,
+              buttonColor: Colors.white,
+              textColor: Colors.black,
+            ),
+            CalculatorButton(
+              buttonText: '9',
+              buttonOnPressed: btnOnClick,
+              buttonColor: Colors.white,
+              textColor: Colors.black,
+            ),
+            CalculatorButton(
+              buttonText: '/',
+              buttonOnPressed: btnOnClick,
+              buttonColor: Colors.purpleAccent,
+              textColor: Colors.black,
+            )
+          ]),
+          Row(children: [
+            CalculatorButton(
+              buttonText: '4',
+              buttonOnPressed: btnOnClick,
+              buttonColor: Colors.white,
+              textColor: Colors.black,
+            ),
+            CalculatorButton(
+              buttonText: '5',
+              buttonOnPressed: btnOnClick,
+              buttonColor: Colors.white,
+              textColor: Colors.black,
+            ),
+            CalculatorButton(
+              buttonText: '6',
+              buttonOnPressed: btnOnClick,
+              buttonColor: Colors.white,
+              textColor: Colors.black,
+            ),
+            CalculatorButton(
+              buttonText: '-',
+              buttonOnPressed: btnOnClick,
+              buttonColor: Colors.purpleAccent,
+              textColor: Colors.black,
+            )
+          ]),
+          Row(children: [
+            CalculatorButton(
+              buttonText: '1',
+              buttonOnPressed: btnOnClick,
+              buttonColor: Colors.white,
+              textColor: Colors.black,
+            ),
+            CalculatorButton(
+              buttonText: '2',
+              buttonOnPressed: btnOnClick,
+              buttonColor: Colors.white,
+              textColor: Colors.black,
+            ),
+            CalculatorButton(
+              buttonText: '3',
+              buttonOnPressed: btnOnClick,
+              buttonColor: Colors.white,
+              textColor: Colors.black,
+            ),
+            CalculatorButton(
+              buttonText: '+',
+              buttonOnPressed: btnOnClick,
+              buttonColor: Colors.purpleAccent,
+              textColor: Colors.black,
+            )
+          ]),
           Row(children: [
             CalculatorButton(
               buttonText: '.',
@@ -105,127 +203,23 @@ class _CalculatorScreenState extends State<CalculatorScreen> {
             ),
             CalculatorButton(
               buttonText: '0',
-              buttonOnPressed: () {},
+              buttonOnPressed: btnOnClick,
               buttonColor: Colors.white,
               textColor: Colors.black,
             ),
             CalculatorButton(
               buttonText: '<',
-              buttonOnPressed: () {},
+              buttonOnPressed: btnOnClick,
               buttonColor: Colors.white,
               textColor: Colors.black,
             ),
             CalculatorButton(
               buttonText: '=',
-              buttonOnPressed: () {},
-              buttonColor: Colors.white,
+              buttonOnPressed: btnOnClick,
+              buttonColor: Colors.deepPurple,
               textColor: Colors.black,
             )
           ]),
-          Row(children: [
-            CalculatorButton(
-              buttonText: '1',
-              buttonOnPressed: () {},
-              buttonColor: Colors.white,
-              textColor: Colors.black,
-            ),
-            CalculatorButton(
-              buttonText: '2',
-              buttonOnPressed: () {},
-              buttonColor: Colors.white,
-              textColor: Colors.black,
-            ),
-            CalculatorButton(
-              buttonText: '3',
-              buttonOnPressed: () {},
-              buttonColor: Colors.white,
-              textColor: Colors.black,
-            ),
-            CalculatorButton(
-              buttonText: '+',
-              buttonOnPressed: () {},
-              buttonColor: Colors.white,
-              textColor: Colors.black,
-            )
-          ]),
-          Row(children: [
-            CalculatorButton(
-              buttonText: '4',
-              buttonOnPressed: () {},
-              buttonColor: Colors.white,
-              textColor: Colors.black,
-            ),
-            CalculatorButton(
-              buttonText: '5',
-              buttonOnPressed: () {},
-              buttonColor: Colors.white,
-              textColor: Colors.black,
-            ),
-            CalculatorButton(
-              buttonText: '6',
-              buttonOnPressed: () {},
-              buttonColor: Colors.white,
-              textColor: Colors.black,
-            ),
-            CalculatorButton(
-              buttonText: '-',
-              buttonOnPressed: () {},
-              buttonColor: Colors.white,
-              textColor: Colors.black,
-            )
-          ]),
-          Row(children: [
-            CalculatorButton(
-              buttonText: '7',
-              buttonOnPressed: () {},
-              buttonColor: Colors.white,
-              textColor: Colors.black,
-            ),
-            CalculatorButton(
-              buttonText: '8',
-              buttonOnPressed: () {},
-              buttonColor: Colors.white,
-              textColor: Colors.black,
-            ),
-            CalculatorButton(
-              buttonText: '9',
-              buttonOnPressed: () {},
-              buttonColor: Colors.white,
-              textColor: Colors.black,
-            ),
-            CalculatorButton(
-              buttonText: '/',
-              buttonOnPressed: () {},
-              buttonColor: Colors.white,
-              textColor: Colors.black,
-            )
-          ]),
-          Row(children: [
-            CalculatorButton(
-              buttonText: 'C',
-              buttonOnPressed: () {},
-              buttonColor: Colors.white,
-              textColor: Colors.black,
-            ),
-            CalculatorButton(
-              buttonText: '(',
-              buttonOnPressed: () {},
-              buttonColor: Colors.white,
-              textColor: Colors.black,
-            ),
-            CalculatorButton(
-              buttonText: ')',
-              buttonOnPressed: () {},
-              buttonColor: Colors.white,
-              textColor: Colors.black,
-            ),
-            CalculatorButton(
-              buttonText: '*',
-              buttonOnPressed: () {},
-              buttonColor: Colors.white,
-              textColor: Colors.black,
-            )
-          ])
         ],
       ),
     );
@@ -247,13 +241,15 @@ class CalculatorButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 10,
-      width: 10,
+      margin: const EdgeInsets.all(10),
+      height: 70,
+      width: 70,
       decoration: BoxDecoration(
-          color: buttonColor, borderRadius: BorderRadius.circular(15)),
+          color: buttonColor, borderRadius: BorderRadius.circular(85)),
       child: TextButton(
         onPressed: () => buttonOnPressed(buttonText),
-        child: Text(buttonText, style: TextStyle(color: textColor)),
+        child:
+            Text(buttonText, style: TextStyle(color: textColor, fontSize: 15)),
       ),
     );
   }
